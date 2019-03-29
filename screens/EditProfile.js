084b88a81d111030483334985b7893ce421be171
Stacy Phan <stacypha@usc.edu>
2019-03-28 20:31:29 -0700

    Profile pages

---
+++ screens/EditProfile.js
@@ -0,0 +1,92 @@
+import React, {Component} from 'react';
+import { Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textInput: {
+    height: 20,
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+  saveButton: {
+    color: '#F78C74',
+  },
+});
+
+export default class EditProfile extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      pw: '',
+      dob: '',
+      des: '',
+    }
+
+    this.saveChanges = this.saveChanges.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handlePwChange = this.handlePwChange.bind(this);
+    this.handbleDobChange = this.handbleDobChange.bind(this);
+    this.handleDesChange = this.handleDesChange.bind(this);
+  }
+  saveChanges() {
+    // TO DO: Make ajax call to send back data changes
+  }
+
+  handleNameChange(name) {
+    this.setState({ name });
+  }
+  handlePwChange(pw) {
+    this.setState({ pw });
+  }
+  handleDobChange(dob) {
+    this.setState({ dob });
+  }
+  handleDesChange(des) {
+    this.setState({ des });
+  }
+
+  render() {
+    return (
+        <View style = {styles.container}>
+          <Text> Create Your Profile </Text>
+          <TextInput style={ styles.textInput }
+            autoCapitalize='words'
+            autoCorrect={false}
+            returnKeyType="next"
+            placeholder='Display Name'
+            value={ this.state.name }
+            onchangeText= { this.handleNameChange }
+          />
+          <TextInput style={ styles.textInput }
+            autoCapitalize='none'
+            returnKeyType="next"
+            placeholder='Password'
+            value={ this.state.pw }
+            onchangeText= { this.handlePwChange }
+          />
+          <TextInput style={ styles.textInput }
+            keyboardType='numbers-and-punctuation'
+            returnKeyType="next"
+            placeholder='Date of Birth (01-31-2019)'
+          />
+          <TextInput style={ styles.textInput }
+            returnKeyType="go"
+            placeholder='Description (optional)'
+          />
+          <Button style= { styles.saveButton }
+            onPress={ saveChanges }
+            title='Save'
+          />
+        </View>
+    );
+  }
+}
+
---
+++ screens/SavedProfile.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import { Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { useScreens } from 'react-native-screens';
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textInput: {
+    height: 20,
+    borderColor: 'gray',
+    borderWidth: 1,
+  }
+});
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // make api call
+      name: '',
+      pw: '',
+      dob: '',
+      des: '',
+    }
+  }
+
+  render() {
+    return (
+        <View style = {styles.container}>
+          <Text> Your Name </Text>
+          <Text> Description </Text>
+          <Text> Playlist count and friend count </Text>
+          <Text> Your Name </Text>
+          <Text> Password </Text>
+          <Text> Birthdate </Text>
+          <Text> Description </Text>
+        </View>
+    );
+  }
+}
+
