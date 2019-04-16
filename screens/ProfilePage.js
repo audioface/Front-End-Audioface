198bd86a3c8b10e54000b74c82faba19219d75da
Basil Chatha <bchatha@usc.edu>
2019-04-16 11:06:46 -0700

    Add files via upload

---
+++ screens/ProfilePage.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+class ProfileItem extends React.Component {
+    
+    constructor() {
+        super();
+
+        this.state = {
+            editClicked: false,
+            myText: ''
+        }
+    }
+
+    editItem() {
+        this.setState({
+            editClicked: true
+        });
+    }
+
+    render() {
+        if(this.state.editClicked) {
+            return(
+                <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', padding: 10, width: '100%' }}>
+                    <TextInput
+                        style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: '50%', paddingLeft: '2%', fontSize: 18 }}
+                        placeholder={this.props.itemType}
+                        value={ this.state.myText }
+                        onChangeText={ (text) => this.setState({ text }) }
+                    />
+                    <Button title="Edit" onPress={() => this.editItem()}></Button>
+                </View>
+            );
+        } else {
+            return(
+                <View style={{ flex: 1, flexDirection:'row', justifyContent:'space-around', alignItems:'center', padding:10, width:'100%'}}>
+                    <Text style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: '50%', paddingLeft: '2%', fontSize: 18 }}> { this.state.myText } </Text>
+                    <Button title="Edit" onPress={() => this.editItem()}></Button>
+                </View>
+            );
+        }
+    }
+}
+
+export default class ProfilePage extends React.Component {
+    
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+    }
+}
